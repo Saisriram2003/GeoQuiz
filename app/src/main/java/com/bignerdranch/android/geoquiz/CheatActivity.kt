88ea4a1f264c3c7e7c29852099ab8d0e5976b9c7
hrl-2024 <hrl@bu.edu>
2023-10-02 11:04:00 -0400

    Complete "Challenge: Closing Loopholes for Cheaters"(Question 3)

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.bignerdranch.android.geoquiz.databinding.ActivityCheatBinding
 
@@ -11,9 +13,12 @@ const val EXTRA_ANSWER_SHOWN = "com.bignerdranch.android.geoquiz.answer_shown"
 private const val EXTRA_ANSWER_IS_TRUE =
     "com.bignerdranch.android.geoquiz.answer_is_true"
 
+private const val CHEAT = "CHEATACTIVITY"
+
 class CheatActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCheatBinding
+    private val quizViewModel: QuizViewModel by viewModels()
 
     private var answerIsTrue = false
 
@@ -22,19 +27,27 @@ class CheatActivity : AppCompatActivity() {
         binding = ActivityCheatBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
-
         binding.showAnswerButton.setOnClickListener {
-            val answerText = when {
-                answerIsTrue -> R.string.true_button
-                else -> R.string.false_button
-            }
-            binding.answerTextView.setText(answerText)
+            showAnswer()
             setAnswerShownResult(true)
+        }
+
+        if (quizViewModel.isCheater) {
+            showAnswer()
         }
     }
 
+    private fun showAnswer() {
+        answerIsTrue = intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false)
+        val answerText = when {
+            answerIsTrue -> R.string.true_button
+            else -> R.string.false_button
+        }
+        binding.answerTextView.setText(answerText)
+    }
+
     private fun setAnswerShownResult(isAnswerShown: Boolean) {
+        quizViewModel.isCheater = isAnswerShown
         val data = Intent().apply {
             putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown)
         }
